33f69ed27efe6870235851944fcb08611a8aaf56
fabien townsend <fabientownsend@gmail.com>
2017-02-23 10:17:11 +0000

    Improve the speed of the computer

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -31,41 +31,51 @@ class Computer
     value_move
   end
 
-  def minimax(board, depth = 9, computer = false)
-    return -depth if board.win?("X")
+  def minimax(board, depth = 9, computer = false, alpha = -100, beta = 100)
+    return -depth if board.win?(opponent_mark)
     return depth if board.win?(mark)
     return 0 if board.tie?
 
     if computer
-      maximizing_player(board, depth)
+      maximizing_player(board, depth, alpha, beta)
     else
-      minimizing_player(board, depth)
+      minimizing_player(board, depth, alpha, beta)
     end
   end
 
-  def maximizing_player(board, depth)
+  def maximizing_player(board, depth, alpha, beta)
     best_value = -100
 
     board.placeholder_available.each do |position|
-      board.set_mark("O", position)
-      value = minimax(board, depth - 1, false)
-      best_value = [best_value, value].max
+      board.set_mark(mark, position)
+      value = minimax(board, depth - 1, false, alpha, beta)
       board.set_mark(nil, position)
+
+      best_value = [best_value, value].max
+      alpha = [alpha, best_value].max
+      break if alpha >= beta
     end
 
     best_value
   end
 
-  def minimizing_player(board, depth)
+  def minimizing_player(board, depth, alpha, beta)
     best_value = 100
 
     board.placeholder_available.each do |position|
-      board.set_mark("X", position)
-      value = minimax(board, depth - 1, true)
-      best_value = [best_value, value].min
+      board.set_mark(opponent_mark, position)
+      value = minimax(board, depth - 1, true, alpha, beta)
       board.set_mark(nil, position)
+
+      best_value = [best_value, value].min
+      beta = [beta, best_value].min
+      break if alpha >= beta
     end
 
     best_value
+  end
+
+  def opponent_mark
+    mark == "X" ? "O" : "X"
   end
 end
